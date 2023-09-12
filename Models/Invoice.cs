0eb7213d902f78af503bb0cdb53ae0ccd39adfed
MrunaliMagar <mrunalipmagar@gmail.com>
2023-09-12 21:02:51 +0530

    DotNet

---
+++ Models/Invoice.cs
@@ -0,0 +1,39 @@
+﻿using Emart_final.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Emart_final.Models
+{
+    public class Invoice
+    {
+        [Key]
+        [Column("invoice_id")]
+        public int invoiceID { get; set; }
+
+        [Column("invoice_date")]
+        public DateTime InvoiceDate { get; set; }
+
+        [Column("total_amt")]
+        public double totalAmt { get; set; }
+
+        [Column("tax")]
+        public double tax { get; set; }
+
+        [Column("delivery_charge")]
+        public double deliveryCharge { get; set; }
+
+        [Column("total_bill")]
+        public double TotalBill { get; set; }
+
+        [Column("custid")]
+        public int custID { get; set; }
+
+        [ForeignKey("custID")]
+        public Customer? Customer { get; set; }
+
+        public ICollection<InvoiceDetails>? InvoiceDtList { get; set; }
+        public ICollection<Order>? Olist { get; set; }
+    }
+}
